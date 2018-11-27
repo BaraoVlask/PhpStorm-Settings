828642db222e485f2fca30609a7232badecf72a2
Guilherme Billig <gbillig@allscritps.com.br>
2018-11-27 17:17:12 -0200

    PS-2018.3 <guilh@Scorpion-PC Create ide.general.xml, Meus Atalhos.xml, Unnamed.php, project.default.xml, other.xml, Default.xml, Custom.xml, file.template.settings.xml, code.style.schemes, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, php.xml, keymap.xml, colors.scheme.xml, databaseDrivers.xml, PHP Class Header.php, emmet.xml, laf.xml, PHP File Header.php, User.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/PHP Class Header.php
@@ -0,0 +1,5 @@
+/**
+ * A classe ${FILE_NAME} pertence ao projeto ${PROJECT_NAME}.
+ *
+ * Criado por Guilherme Billig em ${MONTH} de ${YEAR}.
+ */
---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,5 @@
+/**
+ * O arquivo ${FILE_NAME} pertence ao projeto ${PROJECT_NAME}.
+ *
+ * Criado por Guilherme Billig em ${MONTH} de ${YEAR}.
+ */
